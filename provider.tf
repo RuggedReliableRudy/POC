bd96d2772735f509515107e7e2b1c50d16c7269c
nzeremc <52140404+nzeremc@users.noreply.github.com>
2026-02-24 09:35:38 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,11 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-  }
-
-  backend "s3" {
-    bucket = "your-tf-state-bucket-gov"
-    key    = "infra/west/terraform.tfstate"
-    region = "us-gov-west-1"
   }
 }
 
