24269779763e11ecbbd8ec027133e2ad3024bb3d
nzeremc <52140404+nzeremc@users.noreply.github.com>
2026-03-02 08:02:35 -0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,7 @@
-output "rds_endpoint" {
-  value = aws_rds_cluster.postgres.endpoint
+output "node1_endpoint" {
+  value = aws_db_instance.node1.address
 }
 
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.this.name
-}
-output "ecr_repo_uri" {
-  value = aws_ecr_repository.cpeload.repository_url
+output "node2_endpoint" {
+  value = aws_db_instance.node2.address
 }
