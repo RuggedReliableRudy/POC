01fc50af8dbc1a5ac76c336c958a3b051e79cbcd
nzeremc <52140404+nzeremc@users.noreply.github.com>
2026-03-02 11:08:09 -0800

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,26 +1,7 @@
-# -------------------------
-# Networking
-# -------------------------
-
 variable "vpc_id" {
   type        = string
   description = "Existing VPC where ECS and RDS will be deployed"
 }
-
-# These are populated inside main.tf using Terraform-created subnets.
-variable "ecs_subnet_ids" {
-  type        = list(string)
-  description = "Private subnets for ECS tasks (populated from Terraform-created subnets)"
-}
-
-variable "db_subnet_ids" {
-  type        = list(string)
-  description = "Private subnets for the RDS DB subnet group (populated from Terraform-created subnets)"
-}
-
-# -------------------------
-# ECS Deployment
-# -------------------------
 
 variable "desired_count" {
   type    = number
