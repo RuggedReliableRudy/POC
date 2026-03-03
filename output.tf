bc81c71c35c5145013d56dd565b75f165381c14f
nzeremc <52140404+nzeremc@users.noreply.github.com>
2026-03-03 11:43:06 -0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,2 +1,24 @@
 ecs_subnet_ids = module.network.ecs_private_subnets
 db_subnet_ids  = module.network.rds_private_subnets
+
+
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws-us-gov:iam::018743596699:oidc-provider/token.actions.githubusercontent.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+        },
+        "StringLike": {
+          "token.actions.githubusercontent.com:sub": "repo:ORG/Project-Accumulator:*"
+        }
+      }
+    }
+  ]
+}
