34609710ca67d9a6d92f9db3383a02de77d06947
nzeremc <52140404+nzeremc@users.noreply.github.com>
2026-03-09 09:08:45 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,35 +1,84 @@
-variable "vpc_id" {
+###############################################
+# General Settings
+###############################################
+
+variable "region" {
+  description = "AWS region for all resources"
   type        = string
-  description = "Existing VPC ID"
-}
-
-variable "private_subnet_ids" {
-  type        = list(string)
-  description = "List of existing private subnet IDs"
-}
-
-variable "db_subnet_group_name" {
-  type        = string
-  description = "Existing DB subnet group name"
-}
-
-variable "ecr_uri" {
-  type        = string
-  description = "ECR repository URI for the application image"
-}
-
-variable "image_tag" {
-  type        = string
-  description = "Image tag to deploy"
-}
-
-variable "desired_count" {
-  type        = number
-  default     = 1
-  description = "Number of ECS tasks to run"
 }
 
 variable "account_id" {
+  description = "AWS Account ID (GovCloud format)"
   type        = string
-  description = "AWS GovCloud account ID used in KMS key policy"
+}
+
+###############################################
+# Networking
+###############################################
+
+variable "vpc_id" {
+  description = "ID of the VPC where resources will be deployed"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs for ECS tasks"
+  type        = list(string)
+}
+
+variable "db_subnet_group_name" {
+  description = "Name of the existing DB subnet group"
+  type        = string
+}
+
+###############################################
+# ECS Settings
+###############################################
+
+variable "desired_count" {
+  description = "Desired number of ECS tasks for the cpeload service"
+  type        = number
+  default     = 1
+}
+
+variable "cpeload_image" {
+  description = "Container image URI for the cpeload ECS task"
+  type        = string
+}
+
+variable "sql_runner_image" {
+  description = "Container image URI for the sql-runner ECS task"
+  type        = string
+}
+
+###############################################
+# RDS / Database Settings
+###############################################
+
+variable "db_instance_class" {
+  description = "Instance class for RDS PostgreSQL nodes"
+  type        = string
+  default     = "db.m6g.large"
+}
+
+variable "db_allocated_storage" {
+  description = "Allocated storage (GB) for RDS PostgreSQL nodes"
+  type        = number
+  default     = 100
+}
+
+###############################################
+# Tags
+###############################################
+
+variable "environment" {
+  description = "Environment name (e.g., Dev, QA, Prod)"
+  type        = string
+  default     = "Dev"
+}
+
+variable "repository" {
+  description = "Repository name for tagging"
+  type        = string
+  default     = "Project-Accumulator"
 }
