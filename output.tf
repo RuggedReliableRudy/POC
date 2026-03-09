841478e6cf37f8577fe64768b5c33556e6e21f5e
nzeremc <52140404+nzeremc@users.noreply.github.com>
2026-03-09 09:29:53 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,25 +1,75 @@
-ecs_subnet_ids = module.network.ecs_private_subnets
-db_subnet_ids  = module.network.rds_private_subnets
+###############################################
+# VPC & Networking Outputs
+###############################################
+output "vpc_id" {
+  description = "The VPC ID used for the infrastructure"
+  value       = var.vpc_id
+}
 
+output "private_subnet_ids" {
+  description = "Private subnets used by ECS tasks"
+  value       = var.private_subnet_ids
+}
 
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "arn:aws-us-gov:iam::018743596699:oidc-provider/token.actions.va.ghe.com"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "token.actions.va.ghe.com:aud": "sts.amazonaws.com"
-        },
-        "StringLike": {
-          "token.actions.va.ghe.com:sub": "repo:software/Project-Accumulator:*"
-        }
-      }
-    }
-  ]
+###############################################
+# RDS Outputs
+###############################################
+output "rds_node1_endpoint" {
+  description = "Primary RDS PostgreSQL node endpoint"
+  value       = aws_db_instance.node1.address
+}
+
+output "rds_node2_endpoint" {
+  description = "Secondary RDS PostgreSQL node endpoint"
+  value       = aws_db_instance.node2.address
+}
+
+###############################################
+# ECS Outputs
+###############################################
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.this.name
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.cpeload.name
+}
+
+output "ecs_task_definition_cpeload" {
+  description = "ARN of the cpeload ECS task definition"
+  value       = aws_ecs_task_definition.cpeload.arn
+}
+
+output "ecs_task_definition_sql_runner" {
+  description = "ARN of the sql-runner ECS task definition"
+  value       = aws_ecs_task_definition.sql_runner.arn
+}
+
+###############################################
+# ECR Image Outputs
+###############################################
+output "cpeload_image" {
+  description = "Full ECR image URI used for the cpeload task"
+  value       = var.cpeload_image
+}
+
+output "sql_runner_image" {
+  description = "Full ECR image URI used for the sql-runner task"
+  value       = var.sql_runner_image
+}
+
+###############################################
+# Tags / Metadata
+###############################################
+output "environment" {
+  description = "Deployment environment"
+  value       = var.environment
+}
+
+output "repository" {
+  description = "Repository name used for tagging"
+  value       = var.repository
 }
 
