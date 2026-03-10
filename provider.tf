13b4cb8687e8af255e6ea79bae0e78f5b595f068
nzeremc <52140404+nzeremc@users.noreply.github.com>
2026-03-10 16:01:23 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
+###############################################
+# Terraform Settings
+###############################################
 terraform {
   required_version = ">= 1.5.0"
 
@@ -6,9 +9,42 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "~> 1.22"
+    }
   }
 }
 
+###############################################
+# AWS Provider (GovCloud)
+###############################################
 provider "aws" {
-  region = "us-gov-west-1"
+  region = var.region
+}
+
+###############################################
+# PostgreSQL Providers (Node1 & Node2)
+# These depend on RDS instances created in main.tf
+###############################################
+
+provider "postgresql" {
+  alias    = "node1"
+  host     = aws_db_instance.node1.address
+  port     = 5430
+  username = local.db_creds.user
+  password = local.db_creds.password
+  database = local.db_creds.name
+  sslmode  = "require"
+}
+
+provider "postgresql" {
+  alias    = "node2"
+  host     = aws_db_instance.node2.address
+  port     = 5430
+  username = local.db_creds.user
+  password = local.db_creds.password
+  database = local.db_creds.name
+  sslmode  = "require"
 }
