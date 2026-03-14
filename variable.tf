c6b72e2bd34b471ac1d82c237f7d8af9cd2e7e03
nzeremc <enzerem@aol.com>
2026-03-14 07:44:44 -0700

    new code

--- variable.tf
+++ variable.tf
@@ -2,8 +2,17 @@
 # ROOT MODULE VARIABLES
 ############################################################
 
+# Secrets Manager secret containing:
+# { "user": "postgres", "password": "xxxx" }
 variable "db_credentials_secret_name" {
   type        = string
-  description = "Name of Secrets Manager secret containing JSON { username, password }"
+  description = "Secrets Manager secret containing DB credentials"
+  default     = "accumulator"
+}
+
+# Name of the initial database to create in RDS
+variable "db_name" {
+  type        = string
+  description = "Database name for RDS"
   default     = "accumulator"
 }
