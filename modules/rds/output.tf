3415c940bb628a44fff6cd3345e975b1b6f52e97
nzeremc <enzerem@aol.com>
2026-03-15 10:57:10 -0700

    new code

--- modules/rds/output.tf
+++ modules/rds/output.tf
@@ -1,23 +1,19 @@
-############################################################
-# RDS MODULE OUTPUTS
-############################################################
-
-output "db_endpoint_1" {
-  description = "Endpoint for RDS instance node1"
-  value       = aws_db_instance.node1.address
-}
-
-output "db_endpoint_2" {
-  description = "Endpoint for RDS instance node2"
-  value       = aws_db_instance.node2.address
-}
-
-output "rds_sg_id" {
-  description = "Security group ID used by both RDS nodes"
-  value       = aws_security_group.rds_sg.id
+output "db_port" {
+  value = var.db_port
 }
 
 output "db_name" {
-  description = "Database name"
-  value       = var.db_name
+  value = var.db_name
+}
+
+output "rds_sg_id" {
+  value = aws_security_group.rds_sg.id
+}
+
+output "node1_endpoint" {
+  value = aws_db_instance.node1.address
+}
+
+output "node2_endpoint" {
+  value = aws_db_instance.node2.address
 }
